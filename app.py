8ebbabf0cf53651b2f2c35a10b3363928d614626
Jae Ku <jaejunk39@gmail.com>
2020-05-20 22:34:08 -0500

    - Fixed routing structure - Added Spotify search function

--- app.py
+++ app.py
@@ -6,34 +6,44 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
-# from flask_sql
-
 app = Flask(__name__)
-
 google_api_key = 'AIzaSyC7rX_hVNjF2MH2uQM4StN7tDtkHd0AqAk'
 app.config['SECRET_KEY'] = 'b87cd65425cbfb553740894dcc2fd0fe'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
 db = SQLAlchemy(app)
 
-#Database classes: A collection can have many songs, a song can have many posts
+
+# Database classes: A collection can have many songs, a song can have many posts
 class Collection(db.Model):
     id = db.Column(db.String, primary_key=True)
-    songs = db.relationship('Song', backref='collection')
+    songs = db.relationship('Song', backref='collection', lazy=True)
+
+    def __repr__(self):
+        return f"Collection('{self.id}')"
+
 
 class Song(db.Model):
     song_uri = db.Column(db.String, primary_key=True)
     song_title = db.Column(db.String, nullable=False)
     album_picture = db.Column(db.String, nullable=False)
-    users = db.relationship('Post', backref='song')
-    collection_id = db.Column(db.String, db.ForeignKey('collection_id'), nullable=False)
+    users = db.relationship('Post', backref='song', lazy=True)
+    collection_id = db.Column(db.String, db.ForeignKey('collection.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Song('{self.song_uri}', '{self.song_title}')"
+
 
 class Post(db.Model):
     post_id = db.Column(db.Integer, primary_key=True)
+    post_title = db.Column(db.String(100))
     post_picture = db.Column(db.String)
     post_content = db.Column(db.Text)
     date_posted = db.Column(db.DateTime, default=datetime.utcnow)
-    song_id = db.Column(db.String, db.ForeignKey('song_id'), nullable=False)
+    song_id = db.Column(db.String, db.ForeignKey('song.song_uri'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.post_id}', '{self.date_posted}')"
+
 
 # TODO add data required validator
 class SearchForm(FlaskForm):
@@ -55,13 +65,14 @@ def home():
                                id=user_json.get("id"),
                                email=user_json.get("email"),
                                images=user_json.get("images"),
-                               access_token = session['access_token'],
-                               token_type = session['token_type'],
-                               refresh_token = session['refresh_token'],
+                               access_token=session['access_token'],
+                               token_type=session['token_type'],
+                               refresh_token=session['refresh_token'],
                                login_authorized=True
                                )
     else:
         return render_template('home.html', login_authorized=False)
+
 
 @app.route('/logout', methods=['GET'])
 def logout():
@@ -70,12 +81,14 @@ def logout():
         session.pop(key)
     return redirect(url_for('home'))
 
+
 @app.route('/authorize', methods=['GET'])
 def authorize():
     return redirect(
         'https://accounts.spotify.com/authorize?client_id=2bf4792df3c4489bb9720d3346d63f53&response_type=code'
         '&redirect_uri=http%3A%2F%2F127.0.0.1%3A5000%2Fcallback%2F&scope=user-read-private%20user-read-email%20user'
         '-top-read%20')
+
 
 @app.route('/callback/', methods=['GET', 'POST'])
 def callback():
@@ -101,20 +114,22 @@ def callback():
         session['login_authorized'] = True
         return redirect(url_for('home'))
 
+
 # TODO write function to refresh access token if expired
 @app.route('/refreshaccesstoken')
 def refresh_access_token(refresh_token):
     pass
 
-@app.route('/near', methods=['GET', 'POST'])
+
+@app.route('/searchnear', methods=['GET', 'POST'])
 def near():
     search_form = SearchForm()
     if search_form.validate_on_submit():
         return redirect(url_for('search_near', input_location=search_form.search.data))
-    return render_template('near.html', title='Search Near', form=search_form)
+    return render_template('search.html', title='Search Near', form=search_form, search_type='Near')
 
 
-@app.route('/place/<string:input_location>', methods=['GET', 'POST'])
+@app.route('/searchnear/<string:input_location>', methods=['GET', 'POST'])
 def search_near(input_location):
     # Approximate user geolocation with ip address
     coords = geocoder.ip('me').latlng
@@ -130,7 +145,7 @@ def search_near(input_location):
     # If results is empty list (no results found), we want to ask again or give option to find somewhere far away
     # Redirect to new route?
     if not results['results']:
-        return render_template('search_results.html', title='Oops', error=True)
+        return render_template('place_search_results.html', title='Oops', error=True)
     else:
         # Create a list of locations, most relevant location comes first
         location_list = []
@@ -140,17 +155,36 @@ def search_near(input_location):
                              'longitude': location['geometry']['location']['lng'], 'name': location['name'],
                              'place_id': location['place_id']}
             location_list.append(location_dict)
-    return render_template('search_results.html', title='Search Results', location_list=location_list)
+    return render_template('place_search_results.html', title='Search Results', location_list=location_list)
 
 
-@app.route('/home/<string:place_id>', methods=['GET', 'POST'])
-def location(place_id):
-    return render_template('place.html', place_id=place_id)
+@app.route('/collection/<string:place_id>/address/<string:address>', methods=['GET', 'POST'])
+def location(place_id, address):
+    # Route to display
+    # If collection doesn't exit in the database yet
+    if Collection.query.filter_by(id=place_id).first() is None:
+        return render_template('place.html', place_id=place_id, in_db=False, address=address)
+    else:
+        songs = Song.query.filter_by(collection_id=place_id)
+        return render_template('place.html', in_db=True, songs=songs, place_id=place_id, address=address)
 
 
-# @app.route('/<string:place_id>/<string:music_search>/)
-#
-# @app.route('/<string:place_id>/<string:music_search>/)
+@app.route('/collection/<string:place_id>/searchmusic', methods=['GET', 'POST'])
+def search_music(place_id):
+    search_form = SearchForm()
+    if search_form.validate_on_submit():
+        return redirect(url_for('music_search_results', input_music=search_form.search.data, place_id=place_id))
+    return render_template('search.html', title='Search Music', form=search_form, search_type='Music')
+
+
+@app.route('/collection/<string:place_id>/musicsearchresults/<string:input_music>')
+def music_search_results(place_id, input_music):
+    search_request = requests.get('https://api.spotify.com/v1/search',
+                                  headers={'Authorization': 'Bearer ' + session['access_token']},
+                                  params={'q': input_music}
+                                  )
+    search_json = search_request.json()
+    return render_template('music_search_results.html', search_json=search_json)
 
 
 # @app.route('/home/<str:user_id>')
